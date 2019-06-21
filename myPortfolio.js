a23771eaa35a3b6a03e4ee81b7f2d20d9a0f8de6
oota <shark747474@gmail.com>
2019-06-21 23:49:33 +0900

    add fadein func

--- myPortfolio.js
+++ myPortfolio.js
@@ -2,14 +2,24 @@
 var windowHeight = $(window).height();
 var proTableTop = $('.programming-experience').offset().top
 var confTableTop = $('.conference-experience').offset().top
+var projectMainTop = $('.project-main').offset().top
+var contactMainTop = $('.contact-main').offset().top
 
+
+$('.home-main').animate({opacity:1},2000);
 
 $(window).scroll(function() {
+
   if (($(this).scrollTop() + windowHeight*5/6) > proTableTop) {
     $('.programming-experience').animate({opacity:1},2000);
-
   }
   if (($(this).scrollTop() + windowHeight*5/6) > confTableTop) {
     $('.conference-experience').animate({opacity:1},2000);
+  }
+  if (($(this).scrollTop() + windowHeight*5/6) > projectMainTop) {
+    $('.project-main').animate({opacity:1},2000);
+  }
+  if (($(this).scrollTop() + windowHeight*5/6) > contactMainTop) {
+    $('.contact-main').animate({opacity:1},2000);
   }
 })
